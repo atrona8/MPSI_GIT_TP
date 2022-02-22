91c35bd6f183be726c91bf7e4151c50b15d114d4
Papa Omar SYLLA <atrona8@gmail.com>
2022-02-22 16:31:30 +0000

    Adding files

---
+++ main.py
@@ -0,0 +1,34 @@
+from random import randint
+
+
+Stack = {}
+
+def randomChoice():
+        return randint(2, 15)
+
+def holdInfoInStack(name: str,age: int,country: str , option: int):
+ Stack[name.lower()] = [name,age,country,option]
+
+def divider(n=20):
+ print('-'*n)
+
+def displayinfo(name: str,age: int,country: str)->tuple:
+ holdInfoInStack (name,age,country,1)
+ return name,age,country
+
+def displayinfoByInput(name: str,age: int,country: str)->tuple:
+ holdInfoInStack (name,age,country,2)
+ return displayinfo(name,age,country)
+
+
+if __name__ == '__main__':
+ name = "ESMT"
+ age =30
+ country = "senegal"
+ divider()
+ print(displayinfo(name,age,country))
+ name2 = input(" Enter a name :")
+ age2 =int(input(" Enter a age "))
+ country2 = input("  Enter a country ")
+ divider()
+ print(displayinfoByInput(name2,age2,country2))
