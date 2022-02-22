3db37ce46534737cf03ec8c0d2fc6d5dfa22953f
Papa Omar SYLLA <atrona8@gmail.com>
2022-02-22 12:35:06 +0000

    Create main.py and modify hi content

---
+++ main.py
@@ -0,0 +1,8 @@
+nom = input( "Entrez votre nom svp: ")
+prenom = input( "Entrez votre prenom svp: ")
+age = input( "Entrez votre age svp: ")
+nationnalite = input( "Entrez votre nationnalité svp: ")
+sexe = input( "Entrez votre sexe svp: ")
+
+if __name__ == '__main__':
+    print("Bonjour, je m'appelle ",nom," ",prenom,". Je suis un ",sexe," ",nationnalite,".")
